925c1e3ca8c8d5e8d1d9fed70b5066f31304696c
Barry <barry@Barrys-MacBook-Pro.local>
2013-12-23 15:25:09 -0500

    Initial Commit

---
+++ BicJTS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BicJTS
+//
+//  Created by Barry on 12/23/13.
+//  Copyright (c) 2013 BICSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
